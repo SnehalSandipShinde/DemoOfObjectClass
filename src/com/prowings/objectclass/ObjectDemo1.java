7b93029a17ed42b96022235a398dd7b93d29d1f6
SnehalSandipShinde <snehalshinde2209@gmail.com>
2022-09-13 15:35:46 +0530

    first commit

---
+++ src/com/prowings/objectclass/ObjectDemo1.java
@@ -0,0 +1,31 @@
+package com.prowings.objectclass;
+
+public class ObjectDemo1 {
+	public static void main(String[] args) {
+		Object o = new Object();
+		String pkg = o.getClass().getName();
+		
+		System.out.println(pkg);
+		
+		Object o1 = new Object();
+		Object o3 = new Object();
+//		o1 = o3;
+		
+		System.out.println(o1.equals(o3));
+		System.out.println(o1.hashCode());
+		System.out.println(o3.hashCode());
+		System.out.println(o1 == o3);
+		
+		Object o4 = "name";
+		Object o5 = "Name";
+		o4 = o5;
+
+		System.out.println(o4.equals(o5));
+		System.out.println(o4.hashCode());
+		System.out.println(o5.hashCode());
+		System.out.println(o4 == o5);
+			
+	
+	}
+
+}
