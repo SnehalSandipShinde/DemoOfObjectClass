5cd704e85d57bf14c8a2645efa684c8343a62902
SnehalSandipShinde <113037570+SnehalSandipShinde@users.noreply.github.com>
2022-09-13 15:38:36 +0530

    Update ObjectDemo1.java

--- src/com/prowings/objectclass/ObjectDemo1.java
+++ src/com/prowings/objectclass/ObjectDemo1.java
@@ -23,7 +23,7 @@ public class ObjectDemo1 {
 		System.out.println(o4.equals(o5));
 		System.out.println(o4.hashCode());
 		System.out.println(o5.hashCode());
-		System.out.println(o4 == o5);
+		//System.out.println(o4 == o5);
 			
 	
 	}
